faad87127a60e0a7f4a8f57922d8ad6dd419815c
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-12-17 17:03:46 +0200

    Initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,27 @@
+export default function About() {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        maiores ipsa soluta praesentium quo necessitatibus minima illo
+        similique, reprehenderit eligendi excepturi veniam dolor, repellat animi
+        ab dolorem a. Rerum, minus!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt hic
+        reprehenderit recusandae mollitia placeat libero maxime cupiditate ipsa
+        culpa, repellendus nobis sint minus facilis odit tenetur vitae cumque?
+        Id vitae consequuntur quae molestias temporibus et accusamus, doloremque
+        similique laudantium ex inventore unde totam expedita ratione voluptatum
+        repellendus dolorum reiciendis explicabo nobis beatae earum tempora,
+        animi mollitia. Accusamus, laboriosam culpa distinctio, placeat impedit
+        odio aliquid minima laborum perspiciatis vel, optio dicta magni. Impedit
+        quae saepe animi voluptatibus neque unde cum porro, similique
+        repellendus modi recusandae cumque! Autem nesciunt voluptas culpa,
+        cupiditate dolorum obcaecati voluptatem excepturi iusto sit et quam,
+        dignissimos ut?
+      </p>
+    </main>
+  );
+}
